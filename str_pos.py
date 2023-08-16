257be032b9c94371d3e77494e47f828ce386b44e
Kamente <justinm48000@gmail.com>
2023-08-16 15:38:45 +0300

    done

---
+++ str_pos.py
@@ -0,0 +1,12 @@
+def encode(i):
+    result = ""
+    for char in i:
+        if char.isalpha():
+            position = ord(char.lower()) - ord('a') + 1
+            result += str(position)
+        else:
+            result += char
+    return result
+
+# example:
+print(encode("adi"))
