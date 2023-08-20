2580562f2c2c47feb22ed3f28edf706f085d3df4
Kamente <justinm48000@gmail.com>
2023-08-20 22:14:03 +0300

    print out

---
+++ amicable.py
@@ -0,0 +1,16 @@
+def amicableNumbers(num1, num2):
+    sum1=0
+    sum2=0
+    for i in range(1,num1):
+        if num1%i==0:
+            sum1+=i
+    for j in range(1,num2):
+        if num2%j==0:
+            sum2+=j
+    if(sum1==num2 and sum2==num1):
+        return True
+    else:
+        return False
+
+results = amicableNumbers(220,284)
+print(results)
