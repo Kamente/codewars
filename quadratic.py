135eee639f69a50a47136c66f5234e7f8503ffc6
Kamente <justinm48000@gmail.com>
2023-08-23 15:53:07 +0300

    quadratic.py

---
+++ quadratic.py
@@ -0,0 +1,14 @@
+import math
+
+def quadratic_formula(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant >= 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return [root1, root2]
+    else:
+        raise ValueError("Equation has complex roots")
+
+roots = quadratic_formula(3, 20, 4) # the roots inputs
+print(roots)
+
